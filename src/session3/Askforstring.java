3c60a6a9a640ef6682fc28ee5cdc3066ff33067d
Ruby K <rubykdec22@gmail.com>
2023-05-22 18:10:13 -0500

    Committed

---
+++ src/session3/Askforstring.java
@@ -0,0 +1,21 @@
+package session3;
+
+public class Askforstring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String a = "hello";
+		System.out.println("Hello");
+		System.out.println(a.toUpperCase());
+		String b = a.toUpperCase();
+		System.out.println(a.length());
+		System.out.println( b.replace('O', 'Z'));
+		String c = b.replace('O', 'Z');
+		System.out.println( c.charAt(4));
+		System.out.println(a + " test");
+
+		
+	}
+
+}
